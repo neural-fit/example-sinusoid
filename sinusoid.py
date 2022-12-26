85dddde3b3511d3597eeb2b9bc36b4629af172ec
Thomas Wagenaar <wagenaartje@protonmail.com>
2022-12-26 17:05:24 +0100

    Create sinusoid.py

---
+++ sinusoid.py
@@ -0,0 +1,22 @@
+import neuralfit as nf
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define the dataset
+x = np.linspace(0, 2*np.pi, 1000)
+y = np.sin(x)
+
+# Create and compile the model
+model = nf.Model(inputs=1, outputs=1)
+model.compile(optimizer='alpha', loss='mse', monitors=['size'])
+
+# Evolve the model
+model.evolve(x, y, epochs=1000)
+
+# Get model predictions
+y_hat = model.predict(x)
+
+# Plot results
+plt.plot(x, y, label='True', color='k', linestyle='--')
+plt.plot(x, y_hat, label='Predicted',color='#52C560', linewidth=2)
+plt.show()
